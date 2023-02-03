318d29fc671cb3497ca97b2f2d76abbc36d8eb95
Sandeep.kumar <sandeepkumar@analystintelligence.com>
2023-02-03 18:09:07 +0530

    File Name CHanged

---
+++ src/client/SidebarAnalystIntelligence/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AnalystIntelligence from './components/AnalystIntelligence';
+
+ReactDOM.render(<AnalystIntelligence />, document.getElementById('index'));
